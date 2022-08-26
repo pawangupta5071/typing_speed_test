a000ab7692787396d28df1febcb5b61429d8771c
pawan gupta <pawangupta5071@gmail.com>
2022-08-27 00:24:10 +0530

    First commit

---
+++ typing_speed.py
@@ -0,0 +1,13 @@
+import time
+String = 'I am a programmer and what you are'
+wordcount = len(String.split())
+print(String)
+while True:
+    t0 = time.time()
+    inputtext = str(input('Enter the Sentence:'))
+    t1 = time.time()
+    accuracy = len(set(inputtext.split()) & set(String.split()))
+    accuracy = accuracy/wordcount
+    timetaken = t1-t0
+    wpm = wordcount/timetaken
+    print("wpm", wpm, "Accuracy", accuracy, "Timetaken", timetaken)
